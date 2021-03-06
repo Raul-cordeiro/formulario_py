758289e4e919ec6ac394996e357ccaaaf4132b3c
Raul_Cordeiro <raulcordeirocontato@gmail.com>
2021-03-06 19:47:05 -0300

    tentando im,port pandas
    obs:. a biblioteca não atualiza no vc erro de importaçao

--- meus_codigos/tabela.py
+++ meus_codigos/tabela.py
@@ -1,6 +1,5 @@
-import pandas as pd
 import numpy as np
 coluna = 'A B C D E F G'.split()
 linha = ' nome sexo idade estado-civíl data_de_nascimento forma_de_pagamento'.split()
 dados = np.random.randint(len(coluna * len(linha)))
-print(dados)
+print(dados) No module named 'pandas'
