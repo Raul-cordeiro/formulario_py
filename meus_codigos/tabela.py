c8f07c95761a33ada801750e6e408c5816bab4fa
Raul_Cordeiro <raulcordeirocontato@gmail.com>
2021-03-07 12:07:34 -0300

    continua...

--- meus_codigos/tabela.py
+++ meus_codigos/tabela.py
@@ -3,4 +3,3 @@ import pandas as pd
 coluna = 'A B C D E F G'.split()
 linha = ' nome sexo idade estado-civíl data_de_nascimento forma_de_pagamento'.split()
 dados = np.random.randint(len(coluna * len(linha)))
-print(dados)
